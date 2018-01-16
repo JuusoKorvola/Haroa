4caf1a141a1a3a92082877c41f59626d75effcbb
miikavirpio <miika.virpio@gmail.com>
2018-01-16 20:41:36 +0200

    Poistettu turhat

--- app/src/main/java/org/haxi/haroa/Mane.java
+++ app/src/main/java/org/haxi/haroa/Mane.java
@@ -2,25 +2,18 @@ package org.haxi.haroa;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class Mane extends AppCompatActivity {
 
-    private static final String LOG_TAG = Mane.class.getSimpleName();
-    public static final String EXTRA_MESSAGE = "org.haxi.haroa.extra.MESSAGE";
-    private EditText editteksti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +22,6 @@ public class Mane extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        editteksti = (EditText) findViewById(R.id.editti);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         final ArrayList<String> nappisanat = new ArrayList();
         nappisanat.add("CUNT");
@@ -93,10 +76,7 @@ public class Mane extends AppCompatActivity {
     }
 
     public void launchMunaActivity(View view) {
-        Log.d(LOG_TAG, "Painettu painiketta");
         Intent uusintent = new Intent(this, Muna.class);
-        String viesti = editteksti.getText().toString();
-        uusintent.putExtra(EXTRA_MESSAGE, viesti);
         startActivity(uusintent);
     }
 }
