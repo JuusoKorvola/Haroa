f9d9bcc3d71cc784289db37a2c5fec4aa3df88b4
miikavirpio <miika.virpio@gmail.com>
2018-01-20 00:02:07 +0200

    New empty activity

---
+++ app/src/main/java/org/haxi/haroa/kamera.java
@@ -0,0 +1,13 @@
+package org.haxi.haroa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class kamera extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kamera);
+    }
+}
